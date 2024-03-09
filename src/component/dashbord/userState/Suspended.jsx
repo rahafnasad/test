5b9f1add1ea8dbb01672e0e16f3f584326c0303c
abdalazeezS <abdalazeezshahateet@gmail.com>
2024-03-09 20:02:28 +0200

    fix import useTranslation issue in Suspended component

--- src/component/dashbord/userState/Suspended.jsx
+++ src/component/dashbord/userState/Suspended.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ViewData from './ViewData';
+import { useTranslation } from 'react-i18next';
 
 export default function Suspended() {
+  const { t } = useTranslation();
+  
   const data = [
     { type: t("ACTIVE_SUSPENDED.EMAIL"), name: "email" },
     { type: t("ACTIVE_SUSPENDED.PHONE"), name: "phone" },
   ];
-  
+
   return (
     <ViewData data={data} path="/api/v1/dashboard/users?active=false" state="suspended" />
   )
